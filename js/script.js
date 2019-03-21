335b7830bdbb2e84c5983f28b6d4aa2120ed1857
Dagogo Orifama <dagoris2010@gmail.com>
2019-03-21 19:27:04 +0000

    Added explore, create, share, contact sections, footer included. Also the scrollspy and smooth scrolling was added to the navigation

--- js/script.js
+++ js/script.js
@@ -1,2 +1,34 @@
 // jQuery to get full year
-$('#year').text(new Date().getFullYear())
+// $('#year').text(new Date().getFullYear())
+// Get full year
+var date = new Date().getFullYear();
+document.getElementById('year').innerHTML = date;
+
+// Init Scrollspy
+$('body').scrollspy({target: '#navigation'});
+
+// Add smooth scrolling on all links inside the navbar
+$("#navigation a").on('click', function(e) {
+
+  // Make sure this.hash has a value before overriding default behavior
+  if (this.hash !== "") {
+
+    // Prevent default anchor click behavior
+    e.preventDefault();
+
+    // Store hash
+    var hash = this.hash;
+
+    // Using jQuery's animate() method to add smooth page scroll
+    // The optional number (1000) specifies the number of milliseconds it takes to scroll to the specified area
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 1000, function(){
+
+    // Add hash (#) to URL when done scrolling (default click behavior)
+      window.location.hash = hash;
+    });
+
+  } // End if
+
+});
